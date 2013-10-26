283fca7ecfcbdf2629dfa235ea0a90aef687047a
Patryk Koryzna <pkoryzna@gmail.com>
2013-10-26 15:05:55 +0200

    cleanup

--- js/contextMenu.js
+++ js/contextMenu.js
@@ -1,42 +1,27 @@
 (function(){
+    var jd = require("js/jdCommon.js");
 
- 	function createJDUrl (cityId, fromCoords, fromName, toCoords, toName, date, hour, autoSearch){
-	 	var url = "http://jakdojade.pl/?";
-
- 		url += "cid=" + cityId + "&";
- 		if(fromCoords) url += "fc=" + fromCoords + "&";
- 		if(fromName) url += "fn=" + encodeURI(fromName) + "&";
- 		if(toCoords) url += "tc=" + toCoords + "&";
- 		if(toName) url += "tn=" + encodeURI(toName) + "&";
- 		if(hour) url += "h=" + hour + "&";
- 		if(date) url += "d=" + date + "&";
- 		if(autoSearch) url += "as=" + autoSearch;
-
-	 	return url;
-	 }
-
-	function goToJD(info, tab){
-		chrome.storage.sync.get(["cityId"], function (o){
-			if(!(o.cityId)){
-				window.alert("Nie wybrano miasta. Kliknij na przycisk na toolbarze i wybierz miasto.");
-				
-			} else {
-				navigator.geolocation.getCurrentPosition(function (position){
-					var fromCoords = position.coords.latitude
-					 + ":" + position.coords.longitude;
-					var fromName = "Moja lokalizacja (dokł. " + position.coords.accuracy + " m)";
-					var toName = info.selectionText;
-					var autoSearch = true;
-					chrome.tabs.create({url : createJDUrl(o.cityId, fromCoords, fromName, null,
-			 			toName, null, null, autoSearch)});
-					});
-			}
+    function goToJD (info, tab) {
+	chrome.storage.sync.get(["cityId"], function (o){
+	    if(!(o.cityId)){
+		window.alert("Nie wybrano miasta. Kliknij na przycisk na toolbarze i wybierz miasto.");
+	    } else {
+		navigator.geolocation.getCurrentPosition(function (position){
+		    var o = {};
+		    o["fromCoords"] = position.coords.latitude
+			+ ":" + position.coords.longitude;
+		    o["fromName"] = "Moja lokalizacja (dokł. " + position.coords.accuracy + " m)";
+		    o["toName"] = info.selectionText;
+		    o["autoSearch"] = true;
+		    chrome.tabs.create({url : jd.createJDUrl(o)});
 		});
-	}
-
-	chrome.contextMenus.create({
-		"title": "Znajdź trasę do \"%s\" na jakdojade.pl",
-		"contexts": ["selection"],
-		"onclick": goToJD
+	    }
 	});
+    }
+
+    chrome.contextMenus.create({
+	"title": "Znajdź trasę do \"%s\" na jakdojade.pl",
+	"contexts": ["selection"],
+	"onclick": goToJD
+    });
 })();
--- js/popup.js
+++ js/popup.js
@@ -1,133 +1,94 @@
-(function(){
-	var DEBUG = true;
-	var cityId, cityName, fromCoords, fromName, toCoords,
-	 toName, date, hour, autoSearch, useGeo;
+require(["js/settings","js/jdCommon"], function (settings, jd) {
+    var DEBUG = true;
+    var cityId, cityName, fromCoords, fromName, toCoords,
+    toName, date, hour, autoSearch, useGeo;
+    var fromInput, toInput, locationBtn, nowCheckbox, datetimeInput, mainForm;
 
-	var fromInput, toInput, locationBtn, nowCheckbox, datetimeInput, mainForm;
 
-	 function submitForm(){
-	 	toName = toInput.value;
-	 	fromName = fromInput.value;
-	 	if(!($(nowCheckbox).prop("checked"))) {
+    
+    function submitForm(){
+	toName = toInput.value;
+	fromName = fromInput.value;
+	if(!($(nowCheckbox).prop("checked"))) {
 
-	 		date = getJDDate($(datetimeInput).val());
-	 		hour = getJDTime($(datetimeInput).val());
-	 	}
-	 	if(cityId) {
-	 		chrome.tabs.create({url: createJDUrl(cityId, fromCoords, fromName, toCoords,
-	  			toName, date, hour, autoSearch)});
-	 	} else {
-	 		window.alert("Wybierz miasto.");
-	 	}
-	 	return false;
-	 }
+	    date = jd.getJDDate($(datetimeInput).val());
+	    hour = jd.getJDTime($(datetimeInput).val());
+	}
+	if(cityId) {
+	    chrome.tabs.create({url: jd.createJDUrl(cityId, fromCoords, fromName, toCoords,
+	  					 toName, date, hour, autoSearch)});
+	} else {
+	    window.alert("Wybierz miasto.");
+	}
+	return false;
+    }
 
-	 function createJDUrl(cityId, fromCoords, fromName, toCoords,
-	  toName, date, hour, autoSearch){
-	 	var url = "http://jakdojade.pl/?";
 
- 		url += "cid=" + cityId + "&";
- 		if(useGeo) url += "fc=" + fromCoords + "&";
- 		if(fromName) url += "fn=" + encodeURI(fromName) + "&";
- 		if(toCoords) url += "tc=" + toCoords + "&";
- 		if(toName) url += "tn=" + encodeURI(toName) + "&";
- 		if(hour) url += "h=" + hour + "&";
- 		if(date) url += "d=" + date + "&";
- 		if(autoSearch) url += "as=" + autoSearch;
+    function setCity(){
+	cityId = $(this).attr("data-cid");
+	$("#cityName").html(cityName = $(this).attr("data-name"));
+	settings.set({"cityId" : cityId, "cityName" : cityName});
+    }
 
-	 	return url;
-	 }
+    function setGeoDefault(){
+	settings.set({"geoDefault": useGeo});
+    }
 
-	 function getJDDate(datetime){
-	 	// YYYY-MM-DD T   H  H  :  M  M
-	 	// 0123456789 10  11 12 13 14 15
-	 	return "" + datetime.substr(8,2) + "." + datetime.substr(5,2) + "." +
-	 		 datetime.substr(2,2);
+    function disableLocation(){
+	useGeo = false;
+	$(locationBtn).removeClass("btn-success");
+	locationBtn.onclick = getLocation;
+	$(fromInput).prop({disabled: false});
+	fromInput.value = "";
+	settings.set({"geoDefault": useGeo});
+	$(fromInput).focus();
+    }
 
-	 }
+    function getLocation(){
+	navigator.geolocation.getCurrentPosition(positionSuccess);
+	$(locationBtn).addClass("btn-info");
+    }
 
-	 function getJDTime(datetime){
-	 	 return "" + datetime.substr(11,2) + ":" + datetime.substr(14,2);
-	 }
+    function positionSuccess(position){
+	fromCoords = position.coords.latitude + ":" + position.coords.longitude;
+	fromInput.value = "Moja lokalizacja (dokł. " + position.coords.accuracy + " m)";
+	useGeo = true;
+	if(DEBUG) console.log(fromCoords);
+	$(locationBtn).removeClass("btn-info");
+	$(locationBtn).addClass("btn-success");
+	locationBtn.onclick = disableLocation;
+	$(fromInput).prop({disabled: true});
+	settings.set({"geoDefault": useGeo});
+	$(toInput).focus();
+    }
 
-	 function setCity(){
-	 	cityId = $(this).attr("data-cid");
-	 	$("#cityName").html(cityName = $(this).attr("data-name"));
-		console.log($(this).attr("data-name"));
-		chrome.storage.sync.set({"cityId" : cityId, "cityName" : cityName});
-	 }
+    function datetimeToggle(){
+	if ($(nowCheckbox).prop("checked")) {
+	    $(datetimeInput).addClass("hidden");
 
-	 function setGeoDefault(){
-		chrome.storage.sync.set({"geoDefault": useGeo});
-		// if(geoDefault) {getLocation()} else {disableLocation()};
-	 }
+	} else {
+	    $(datetimeInput).removeClass("hidden");
+	    var dateNow = new Date();
+	    function pad(n){ return n<10 ? '0'+ n : n };
+	    $(datetimeInput).val("" + dateNow.getFullYear() + "-" + 
+	 			 pad(dateNow.getMonth()+1) + "-" + pad(dateNow.getDate()) + "T" +
+	 		 	 pad(dateNow.getHours()) + ":" + pad(dateNow.getMinutes()));
+	}
+    }
 
-	 function disableLocation(){
-	 	useGeo = false;
-		$(locationBtn).removeClass("btn-success");
-		locationBtn.onclick = getLocation;
-		$(fromInput).prop({disabled: false});
-		fromInput.value = "";
-		setGeoDefault();
-		$(fromInput).focus();
-	 }
+    function init(){
+	mainForm = document.forms[0];
+	mainForm.onsubmit = submitForm;
+	fromInput = mainForm.from;
+	toInput = mainForm.to;
+	locationBtn = mainForm.useLoc;
+	nowCheckbox = mainForm.now;
+	datetimeInput = mainForm.datetime;
+ 	
+	nowCheckbox.onclick = datetimeToggle;
+	locationBtn.onclick = getLocation;
+	$("#cityMenu a").click(setCity);
+    }
 
-	 function getLocation(){
-	 	navigator.geolocation.getCurrentPosition(positionSuccess);
-		$(locationBtn).addClass("btn-info");
-	 }
-
-	 function positionSuccess(position){
-	 	fromCoords = position.coords.latitude + ":" + position.coords.longitude;
-	 	fromInput.value = "Moja lokalizacja (dokł. " + position.coords.accuracy + " m)";
-	 	useGeo = true;
-	 	if(DEBUG) console.log(fromCoords);
-		$(locationBtn).removeClass("btn-info");
-	 	$(locationBtn).addClass("btn-success");
-	 	locationBtn.onclick = disableLocation;
-	 	$(fromInput).prop({disabled: true});
-	 	setGeoDefault();
-	 	$(toInput).focus();
-	 }
-
-	 function datetimeToggle(){
-	 	if ($(nowCheckbox).prop("checked")) {
-	 		$(datetimeInput).addClass("hidden");
-
-	 	} else {
-	 		$(datetimeInput).removeClass("hidden");
-			var dateNow = new Date();
-			function pad(n){ return n<10 ? '0'+ n : n };
-	 		$(datetimeInput).val("" + dateNow.getFullYear() + "-" + 
-	 			pad(dateNow.getMonth()+1) + "-" + pad(dateNow.getDate()) + "T" +
-	 		 	pad(dateNow.getHours()) + ":" + pad(dateNow.getMinutes()));
-	 	}
-	 }
-
-	 (function init(){
-		mainForm = document.forms[0];
-	 	mainForm.onsubmit = submitForm;
-	 	fromInput = mainForm.from;
-	 	toInput = mainForm.to;
-	 	locationBtn = mainForm.useLoc;
-	 	nowCheckbox = mainForm.now;
-	 	datetimeInput = mainForm.datetime;
- 	    
-	 	nowCheckbox.onclick = datetimeToggle;
-	 	locationBtn.onclick = getLocation;
-	 	$("#cityMenu a").click(setCity);
-	 	chrome.storage.sync.get(["cityId", "cityName", "geoDefault"],
-	 		function(o){
-	 			if(!(chrome.runtime.lastError)){
-		 				cityId = o.cityId;
-		 				$("#cityName").html(cityName = o.cityName);
-		 				if(useGeo = o.geoDefault){
-		 					getLocation();
-		 				} 
-	 				}else{
-	 					console.log(chrome.runtime.lastError.message);
-	 				}
-	 	});
-	 })();
-
-})();
+    init();
+});
